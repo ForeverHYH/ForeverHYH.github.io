90aeb1e2755b3e0b70fb5db867a55dbf3ffb3eed
ForeverHYH <foreverhyh58@gmail.com>
2015-04-28 00:15:50 +0800

    wine test

--- README.md
+++
@@ -1,2 +0,0 @@
-# ForeverHYH.github.io
-Blog or other html page in there
--- js/start-page-controller.js
+++ js/start-page-controller.js
@@ -1,32 +1,27 @@
-/**
- * Created by zhaozijing on 4/17/15.
- */
 
 var refreshMilli = 50;
 var startFrameIndex = 1;
-var startFrame = 70;
+var startFrame = 99;
 var videoBox = $(document.getElementById('startImg')); 
 
 function playStartVideo() {
     if (startFrameIndex >= startFrame) {
         clearInterval(intervalID);
-        startNextAnim();
+        //startNextAnim();
         return;
     }
     $(videoBox).css(
             'background-position', 
             -startFrameIndex*videoBox.width()
         );
+    //videoBox.style.backgroundPosition = -(startFrameIndex*videoBox.width()) + 'px';
     startFrameIndex += 1;
 }
 
 function startPlayStartVideo() {
-    changePage("start");
-    $(".z-start").hide();
     startFrameIndex = 1;
-    $("#first-bg").show();
     // start playing pngs
-    intervalID = setInterval(playStartVideo, refreshMilli*2);
+    intervalID = setInterval(playStartVideo, refreshMilli);
 }
 
 
